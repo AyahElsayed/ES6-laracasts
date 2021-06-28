ac012776c2ac162ee6e97b536ccc1de383f95246
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-06-28 19:43:55 +0200

    template strings

---
+++ template.js
@@ -0,0 +1,11 @@
+// use [] and "" to add multiline string  =>old way
+
+let name = 'Bar';
+
+let template = `
+    <div class="Alert">
+        <p>${name}</p>
+    </div>
+    `;
+
+console.log(template);
